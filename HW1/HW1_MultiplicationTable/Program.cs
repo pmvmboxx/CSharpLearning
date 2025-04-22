1ae8d18a0deee8c2922d3f28cd1a387e3413aea3
Maryna.Ponomarenko <pmvmbox@gmail.com>
2025-04-22 11:01:01 +0300

    Updated HW1

--- HW1/HW1_MultiplicationTable/Program.cs
+++ HW1/HW1_MultiplicationTable/Program.cs
@@ -1,16 +1,65 @@
-﻿namespace HW1_MultiplicationTable
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_UpdatedMultiplicationTable
 {
     internal class Program
     {
+        const int DEFAULT_COLUMN_WIDTH = 15;
+        const int DEFAULT_FROM = 1;
+        const int DEFAULT_TO = 10;
+        const int DEFAULT_COLUMN_START = 1;
+        const int DEFAULT_COLUMN_END = 10;
+        const int DEFAULT_COLUMN_NUMBER = 4;
+        const int DEFAULT_ROW_HEIGHT = 12;
 
         static void Main(string[] args)
         {
-            //TableForLoop();
-            //TableWhileLoop();
-            //TableDoWhileLoop();
-            TableOutput();
-            Console.ReadKey();
+            while (true)
+            {
+                ShowMenu();
+
+                Console.Write("Choose the task (or 0 to exit): ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        TableForLoop();
+                        break;
+                    case "2":
+                        TableWhileLoop();
+                        break;
+                    case "3":
+                        TableDoWhileLoop();
+                        break;
+                    case "4":
+                        Output();
+                        break;
+                    case "0":
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Incorrct input. Try again.\n");
+                        break;
+                }
+            }
         }
+
+        private static void ShowMenu()
+        {
+            Console.WriteLine("\n=== TASK MENU ===");
+            Console.WriteLine("1. Table with For Loop");
+            Console.WriteLine("2. Table with While Loop");
+            Console.WriteLine("3. Table with DoWhile Loop");
+            Console.WriteLine("4. Multiplication Table Task");
+            Console.WriteLine("0. exit");
+            Console.WriteLine();
+        }
+
         private static int? GetInput(int minValue, int maxValue, string message)
         {
             Console.Write(message);
@@ -27,36 +76,31 @@
                 }
                 Console.WriteLine($"Please, enter the correct value between {minValue} and {maxValue}: ");
             } while (true);
-
-
         }
+
         private static void DrawTable(int positionX, int positionY, int tableNumber)
         {
-            var x = positionX;
-            var y = positionY;
-            for (int i = 1; i <= 10; i++)
+            var start_x = positionX;
+            var start_y = positionY;
+
+            for (int i = DEFAULT_COLUMN_START; i <= DEFAULT_COLUMN_END; i++)
             {
-                Console.SetCursorPosition(x, y++);
+                Console.SetCursorPosition(start_x, start_y++);
                 Console.Write($"{tableNumber} * {i} = {tableNumber * i}");
             }
         }
 
-        private static void TableOutput()
+        private static void Output()
         {
-            const int defaultColumnLength = 15;
-            const int defaultFrom = 1;
-            const int defaultTo = 10;
-            const int defaultColumnsNumber = 4;
-            const int defaultRowHeight = 12;
 
-            int from = GetInput(1, 10, "Enter the start point A: ") ?? defaultFrom;
-            int to = GetInput(from, 10, "Enter the end point B: ") ?? defaultTo;
-            int columnNumber = GetInput(1, 9, "Enter the column number: ") ?? defaultColumnsNumber;
+            int from = GetInput(1, 10, "Enter the start point A: ") ?? DEFAULT_FROM;
+            int to = GetInput(from, 10, "Enter the end point B: ") ?? DEFAULT_FROM;
+            int columnNumber = GetInput(1, 9, "Enter the column number: ") ?? DEFAULT_COLUMN_NUMBER;
             int rowNumbers = (int)Math.Ceiling((double)(to - from + 1) / columnNumber);
             int currentNumber = from;
 
             Console.Clear();
-            DrawAllTables(defaultColumnLength, defaultRowHeight, to, columnNumber, rowNumbers, currentNumber);
+            DrawAllTables(DEFAULT_COLUMN_WIDTH, DEFAULT_ROW_HEIGHT, to, columnNumber, rowNumbers, currentNumber);
         }
 
         private static void DrawAllTables(int defaultColumnLength, int defaultRowHeight, int to, int columnNumber, int rowNumbers, int currentNumber)
@@ -79,22 +123,25 @@
             }
         }
 
-        private static void TableDoWhileLoop()
+        private static void TableForLoop()
         {
+            const int columnLength = 15;
             Console.WriteLine("\t\t==== Multiplication Table ==== \t\t");
-            int i = 1;
-            do
+            for (int i = 1; i <= 10; i++)
             {
-                int j = 1;
-                do
+                for (int j = 1; j <= 10; j++)
                 {
                     int res = i * j;
-                    Console.Write($"{j} * {i} = {res}\t");
-                    j++;
-                } while (j <= 10);
-                Console.Write("\n");
-                i++;
-            } while (i <= 10);
+                    var str = $"{j} * {i} = {res}";
+                    var spaceNumbers = columnLength - str.Length;
+                    str += new string(' ', spaceNumbers);
+                    Console.Write(str);
+                    if (j == 10)
+                    {
+                        Console.Write("\n");
+                    }
+                }
+            }
         }
 
         private static void TableWhileLoop()
@@ -115,27 +162,22 @@
             }
         }
 
-
-
-        private static void TableForLoop()
+        private static void TableDoWhileLoop()
         {
-            const int columnLength = 15;
             Console.WriteLine("\t\t==== Multiplication Table ==== \t\t");
-            for (int i = 1; i <= 10; i++)
+            int i = 1;
+            do
             {
-                for (int j = 1; j <= 10; j++)
+                int j = 1;
+                do
                 {
                     int res = i * j;
-                    var str = $"{j} * {i} = {res}";
-                    var spaceNumbers = columnLength - str.Length;
-                    str += new string(' ', spaceNumbers);
-                    Console.Write(str);
-                    if (j == 10)
-                    {
-                        Console.Write("\n");
-                    }
-                }
-            }
+                    Console.Write($"{j} * {i} = {res}\t");
+                    j++;
+                } while (j <= 10);
+                Console.Write("\n");
+                i++;
+            } while (i <= 10);
         }
     }
 }
