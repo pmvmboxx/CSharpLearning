6439a2c2ab08ea40aa307d9e0c1e56a2a64d0d9f
Maryna.Ponomarenko <pmvmbox@gmail.com>
2025-05-25 10:58:16 +0300

    updated HW5

--- 20250511_HW5_WeatherAggregator/WeatherAggregator/Weather.cs
+++ 20250511_HW5_WeatherAggregator/WeatherAggregator/Weather.cs
@@ -10,13 +10,13 @@ namespace WeatherAggregator
     [Flags]
     public enum Weather : byte
     {
-        None = 0,
-        Sunny = 1,
-        Cloudy = 2,
-        Rainy = 4,
-        Snowy = 8,
-        Thunderstorm = 16,
-        Windy = 32,
-        Foggy = 64
+        None         = 0b00000000,   // 0
+        Sunny        = 0b00000001,   // 1
+        Cloudy       = 0b00000010,   // 2
+        Rainy        = 0b00000100,   // 4
+        Snowy        = 0b00001000,   // 8
+        Thunderstorm = 0b00010000,   // 16
+        Windy        = 0b00100000,   // 32
+        Foggy        = 0b01000000    // 64
     }
 }
