a84c916e58159eb6d64f8238afc826458303df03
Maryna.Ponomarenko <pmvmbox@gmail.com>
2025-08-27 18:52:08 +0300

    Updated struct Group

--- StudentManagementSystem/StudentManagementSystem.BL/Group.cs
+++ StudentManagementSystem/StudentManagementSystem.BL/Group.cs
@@ -127,36 +127,6 @@ namespace StudentManagementSystem.BL
             ErrorStatus = Status.OK;
         }
 
-        //TODO: 2?
-        //TODO: передать размер в параметры
-        //TODO: array.resize
-        private void ResizeStudentsArray(int increaseBy = DEFAULT_RESIZE_VALUE)
-        {
-            if (increaseBy <= 0)
-            {
-                ErrorStatus = Status.BadOperation;
-                return;
-            }
-
-            ErrorStatus = Status.BadOperation;
-
-            int newSize = _students.Length + increaseBy;
-            Student[] newArray = new Student[newSize];
-
-            for (int i = 0; i < _students.Length; i++)
-            {
-                newArray[i] = _students[i];
-            }
-
-            _students = newArray;
-
-            ErrorStatus = Status.OK;
-
-#if DEBUG
-            Console.WriteLine($"Resized array: added +{increaseBy} students (new length: {_students.Length}).");
-#endif
-        }
-
 
         //TODO: поиск группы по текстовому полю -> поиск имя\фамилия\ID (схема обработки + как предоставить результат поиска)
         //TODO: запретить измененние номера зачетки, кроме контруктора
@@ -165,73 +135,84 @@ namespace StudentManagementSystem.BL
         //TODO: reduce the number of returns
 
         // R - read
-        public Student FindStudent(long recordBookNumber)
+        public bool FindStudent(long recordBookNumber, out Student foundStudent)
         {
-            //bool found = Tr
-
+            bool isFound;
             int position = FindStudentPosition(recordBookNumber);
+
             if (position != -1)
             {
+                foundStudent = _students[position];  
                 ErrorStatus = Status.OK;
-                return _students[position];
+                isFound = true;                        // студент найден
             }
             else
             {
+                foundStudent = default(Student);    
                 ErrorStatus = Status.NotFound;
-                return default(Student);
+                isFound = false;                       // студент не найден
             }
+
+            return isFound;
         }
 
         public int[] SearchStudents(string searchTerm)
         {
-            List<int> positions = new List<int>();
+            int[] tempPositions = new int[_studentCount]; // тут храним позиции найденных студентов, которые подходят под заданую строку
+            int count = 0; // счетчик для количества найденных студентов
 
+            // проходимся по студентам и ищем совпадения
             for (int i = 0; i < _studentCount; i++)
             {
                 if (_students[i].FirstName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) 
                         || _students[i].LastName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) 
                         || _students[i].RecordBookNumber.ToString().Contains(searchTerm))
                 {
-                    positions.Add(i);
+                    tempPositions[count++] = i; // доабвляем индекс найденного студента в массив
                 }
             }
 
-            if (positions.Count == 0)
+            if (count == 0)
+            {
                 ErrorStatus = Status.NotFound;
+                tempPositions = new int[0]; // ничего не найдено
+            }
             else
+            {
                 ErrorStatus = Status.OK;
+                Array.Resize(ref tempPositions, count); // найдено, и resize к количеству найденных совпадений
+            }
 
-            return positions.ToArray();
+            return tempPositions;
         }
-        // U - update
 
-
-        //TODO: for1 - find(позиция студента), for2 - shift
-        //TODO: использовать для studetn transfer
-        //TODO: enum Status
-        //TODO: array.copy
-        //TODO: returns
+        // U - update?
 
         //D - delete
-        public Student RemoveStudent(long recordBookNumber)
+        public Student? RemoveStudent(long recordBookNumber)
         {
+            Student? removedStudent = null; // deleted student
             int position = FindStudentPosition(recordBookNumber);
+
             if (position != -1)
             {
-                Student removed = _students[position];
-                for (int i = position; i < _studentCount - 1; i++)
+                removedStudent = _students[position];
+
+                // сдвигаем элементы массива, чтобы заполнить "дырку"
+                if (position < _studentCount - 1)
                 {
-                    _students[i] = _students[i + 1];
+                    Array.Copy(_students, position + 1, _students, position, _studentCount - position - 1);
                 }
+
                 _studentCount--;
                 ErrorStatus = Status.OK;
-                return removed;
             }
             else
             {
                 ErrorStatus = Status.NotFound;
-                return default(Student);
             }
+
+            return removedStudent; 
         }
         #endregion
 
